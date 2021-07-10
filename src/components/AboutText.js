7b2a15aae331aa8c7b1399cafd26721df911904a
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-09 22:16:31 -0500

    updating about text component

--- src/components/AboutText.js
+++ src/components/AboutText.js
@@ -3,26 +3,28 @@ import Container from './Container'
 
 export default function AboutText() {
     return (
-        <div className="AboutText text-center">
-            <h3 className="AboutHeading" style={{ "margin-top": "2%" }}>Welcome to Puppies-Tinder!</h3>
-            <p className="AboutTextContent1">It's like Tinder, but with Dogs!</p>
-            <p className="AboutTextContent2">The sad eyes searching for the loving heart, for a friend and companion;
-            the sad stories and a struggle for life… We say: “No, no more sadness” with a dog adoption app Puppies-Tinder
-            that helps dogs find forever homes.</p>
-            <p className="AboutTextContent3">Puppies-Tinder promotes social responsibility, seeks to create an emotionally
-            healthier society and is acting for the animals’ good.</p>
-            <p className="AboutTextContent4">We created Puppies-Tinder in response to a number: 2.5 million.<br></br>
-            That’s how many pets are surrendered to shelters each year because their owners can’t keep them.
-            Shelters don’t have the money or room to care for them. <br></br>
-            They already have 5 million strays and abused animals to take in each year.
-            And so, millions are euthanized.
-            We are determined that things don’t have to be this way.<br></br>
-             Not in our 21st century, web-connected world. So we created Puppies-Tinder, an online community where people who can’t keep their pets connect directly with people who want to adopt a pet.<br></br>
-            Together, we’ll keep dogs and cats from ever seeing the inside of a shelter, so they can love and be loved in new homes.
-            Saving lives. Unburdening shelters. Creating community to make a difference. That’s our mission.<br></br>
-             That’s what we want you to know about us.
-            </p>
-        </div>
+        <Container>
+            <div className="AboutText text-center">
+                <h3 className="AboutHeading" style={{ "margin-top": "2%" }}>Welcome to Puppies-Tinder!</h3>
+                <p className="AboutTextContent1">It's like Tinder, but with Dogs!</p>
+                <p className="AboutTextContent2">The sad eyes searching for the loving heart, for a friend and companion;
+                the sad stories and a struggle for life… We say: “No, no more sadness” with a dog adoption app Puppies-Tinder
+                that helps dogs find forever homes.</p>
+                <p className="AboutTextContent3">Puppies-Tinder promotes social responsibility, seeks to create an emotionally
+                healthier society and is acting for the animals’ good.</p>
+                <p className="AboutTextContent4">We created Puppies-Tinder in response to a number: 2.5 million.<br></br>
+                That’s how many pets are surrendered to shelters each year because their owners can’t keep them.
+                Shelters don’t have the money or room to care for them. <br></br>
+                They already have 5 million strays and abused animals to take in each year.
+                And so, millions are euthanized.
+                We are determined that things don’t have to be this way.<br></br>
+                Not in our 21st century, web-connected world. So we created Puppies-Tinder, an online community where people who can’t keep their pets connect directly with people who want to adopt a pet.<br></br>
+                Together, we’ll keep dogs and cats from ever seeing the inside of a shelter, so they can love and be loved in new homes.
+                Saving lives. Unburdening shelters. Creating community to make a difference. That’s our mission.<br></br>
+                That’s what we want you to know about us.
+                </p>
+            </div>
+        </Container>
         // <Container>
         //     <div className="row">
         //         <div className="col md-12 p-4 text-center">
