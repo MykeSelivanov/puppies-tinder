269eb6dcadf53f1c4002f60744420ca7ee49bc07
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-09 22:15:17 -0500

    adding Discover Page

---
+++ src/pages/DiscoverPage.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import Container from '../components/Container';
+import ImageComponent from '../components/ImageComponent';
+import axios from 'axios';
+
+export default function DiscoverPage() {
+
+    const displayCard = true;
+    const [image, setImage] = useState('');
+    let [match, setMatch] = useState(false);
+    let [matchCount, setMatchCount] = useState(0);
+
+    const getRndDog = () => {
+        axios
+            .get('https://dog.ceo/api/breeds/image/random')
+            .then((res) => {
+                setImage(res.data.message)
+            })
+            .catch(err => 'error');
+    }
+
+    useEffect(() => {
+        getRndDog();
+    }, [matchCount])
+
+    const handleBtnClick = (btnType) => {
+        setMatch(false);
+        if (btnType === 'up') {
+            if (1 === Math.floor(Math.random() * 5) + 1) {
+                setMatch(true);
+                setMatchCount(++matchCount);
+            }
+        } 
+        getRndDog();
+    }
+
+    return (
+        <Container style={{ textAlign: "center" }}>
+            <h2>Make Some Friends</h2>
+            <h3>Thumbs up for the puppies you like!</h3>
+            <ImageComponent displayCard={displayCard} imageSource={image} handleBtnClick={handleBtnClick} />
+            <h1>{matchCount} puppies liked you back so far!</h1>
+            <div style={{ opacity: match ? 1 : 0}} className="alert alert-success" role="alert">Yay! Someone liked you back!</div>
+        </Container>
+    )
+}
