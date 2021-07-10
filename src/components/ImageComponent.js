9f752bac5ccec88c5eb267896f48c2ff7abb523b
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-10 13:52:30 -0500

    updating image component to be used in search page

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Container from '../components/Container';
-
+import {useLocation } from 'react-router-dom'
 export default function ImageComponent(props) {
-    const {displayCard, imageSource, handleBtnClick} = props;
+    const location = useLocation();
+    // console.log(location.pathname);
+
+    const { displayCard, imageSrc, handleBtnClick } = props;
 
     if (displayCard === true) {
         return (
-            <Container style={{display: "flex", justifyContent: "center"}}>
+            <Container style={{ display: "flex", justifyContent: "center" }}>
                 <div className="card">
-                    <img src={imageSource} class="card-img-top" alt="No image" style={{height: "400px", width:"500px"}}/>
+                    <img src={imageSrc} className="card-img-top" alt="Not found" style={{ height: "400px", width: "500px" }} />
                     <div className="card-body">
                         <button type="button" className="btn btn-outline-danger btn-lg ms-5 me-5" onClick={() => handleBtnClick("down")}>Move on</button>
                         <button type="button" className="btn btn-outline-success btn-lg ms-5 me-5" onClick={() => handleBtnClick("up")}>Thumbs up!</button>
@@ -19,9 +22,12 @@ export default function ImageComponent(props) {
     } else {
         return (
             <Container>
-                <div className="card">
-                    <img src={imageSource} class="card-img-top" alt="No image" />
-                </div>
+                {/* <div className="card">
+                    {images.map((image) => {
+                        return <img src={image} className="card-img-top" alt="Not found" />
+                    })} 
+                </div> */}
+                <img src={imageSrc} className="card-img-top m-1" alt="Not found" style={{ height: "400px", width: "500px" }} />
             </Container>
         )
     }
