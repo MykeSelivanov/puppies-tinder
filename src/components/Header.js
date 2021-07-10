e235808785d1335e6f37cde4b1eeb098f6eafeba
MykeSelivanov <michael777selivanov@gmail.com>
2021-07-10 13:49:39 -0500

    fixing class to className attrs

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 export default function Header() {
     return (
         <div className="Header">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="/">Puppies-Tinder</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Puppies-Tinder</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                               <Link class="nav-link" to="/about">About</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/about">About</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link class="nav-link" to="/discover">Discover</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/discover">Discover</Link>
                             </li>
-                            <li class="nav-item">
-                               <Link class="nav-link" to="/search">Search</Link>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/search">Search</Link>
                             </li>
                         </ul>
                     </div>
